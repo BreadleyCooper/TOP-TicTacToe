9d34d6f02e4155d340d9d17501f18d28c787b620
Jordan Ulmer <julmer526@gmail.com>
2022-04-28 16:37:01 +0100

    tie bug fixed. To add - replay button and +styling

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 
 
 const gameBoard = (() => {
+    let won = false;
+    let tie = true;
     // placeholder array for the gameBoard
     let gameBoardGrid = ["","","","","","","","",""]
     // cache the DOM elements needed and render
@@ -10,38 +12,40 @@ const gameBoard = (() => {
     roundInfo.textContent = `${currentPlayer}, it's your turn. You are X`
     
     function toggleWinOverlay (){
-      document.body.classList.toggle("activeForm")
+        document.body.classList.toggle("activeForm")
     }
-    
+    let overlayResult = document.querySelector(".result")
+
     const _createGameBoard = () => {
             
         gameContainer = document.querySelector(".gameContainer");
         for (let i=0;i<gameBoardGrid.length;i++) {
-          
+        
 //          create the game grid in the DOM
-          
+        
             let gridSquare = document.createElement('div');
             gridSquare.setAttribute("id",`${i}`)
             gridSquare.classList.add("gridSquare")
             gameContainer.appendChild(gridSquare)
             gridSquare.addEventListener("click",() => {
-              
+            
 //          logic for inputting an X or O and splicing to the array    
-             
+            
             if (gridSquare.textContent == "") {    
                 if (currentPlayer == "Player 1") {
                     gameBoardGrid.splice(i,1,"X")
                     gridSquare.textContent = playerMarker 
+                    gridSquare.classList.toggle("X")
                     _checkWin(); 
                     currentPlayer = "Player 2";
                     playerMarker = "O"
                     roundInfo.textContent = `${currentPlayer}, it's your turn. You are ${playerMarker}`
                     
-                   
-  
+
                 }else  {currentPlayer = "Player 1";
                         gameBoardGrid.splice(i,1,"O")
-                        gridSquare.textContent = playerMarker 
+                        gridSquare.textContent = playerMarker
+                        gridSquare.classList.toggle("O")
                         _checkWin(); 
                         playerMarker = "X"
                         roundInfo.textContent = `${currentPlayer}, it's your turn. You are ${playerMarker}`;   
@@ -54,10 +58,19 @@ const gameBoard = (() => {
         }
 //      function to check if a winning condition is met. 
     const _checkWin = () => {
-    
-    let won = false
 
-    const win1 = (() => {
+    const checkTie = () => {
+    if (!gameBoardGrid.includes("") && (won == false)) {
+        alert ("tie")
+        tie = true;
+        overlayResult.textContent = "It's a tie!"
+        toggleWinOverlay();
+        return
+    }
+    }
+    
+
+    const win1 = () => {
         let a = gameBoardGrid[0]
         let b = gameBoardGrid[1]
         let c = gameBoardGrid[2]
@@ -71,10 +84,10 @@ const gameBoard = (() => {
             toggleWinOverlay()
             return
             }
-        })()
+        }
     
 
-    const win2 = (() => {
+    const win2 = () => {
         let a = gameBoardGrid[3]
         let b = gameBoardGrid[4]
         let c = gameBoardGrid[5]
@@ -87,10 +100,10 @@ const gameBoard = (() => {
             _win()
             return
             }
-        })()
-        
+        }
+    
 
-    const win3 = (() => {
+    const win3 = () => {
         let a = gameBoardGrid[6]
         let b = gameBoardGrid[7]
         let c = gameBoardGrid[8]
@@ -103,10 +116,10 @@ const gameBoard = (() => {
             _win()
             return
             }
-        })()
+        }
     
     
-    const win4 = (() => {
+    const win4 = () => {
         let e = gameBoardGrid[0]
         let f = gameBoardGrid[3]
         let g = gameBoardGrid[6]
@@ -119,9 +132,9 @@ const gameBoard = (() => {
             _win()
             return
             }
-        })()
+        }
     
-    const win5 = (() => {
+    const win5 = () => {
         let h = gameBoardGrid[1]
         let i = gameBoardGrid[4]
         let j = gameBoardGrid[7]
@@ -134,9 +147,9 @@ const gameBoard = (() => {
             _win()
             return
             }
-        })()
+        }
     
-    const win6 = (() => {
+    const win6 = () => {
         let k = gameBoardGrid[2]
         let l = gameBoardGrid[5]
         let m = gameBoardGrid[8]
@@ -149,9 +162,9 @@ const gameBoard = (() => {
             _win()
             return
             }
-        })()
-    
-    const win7 = (() => {
+        }
+       
+    const win7 = () => {
         let n = gameBoardGrid[6]
         let o = gameBoardGrid[4]
         let p = gameBoardGrid[2]
@@ -164,11 +177,11 @@ const gameBoard = (() => {
             _win()
             return
             }
-        })()
+        }
     
     
     
-    const win8 = (() => {
+       const win8 = () => {
         let q = gameBoardGrid[0]
         let r = gameBoardGrid[4]
         let s = gameBoardGrid[8]
@@ -176,28 +189,35 @@ const gameBoard = (() => {
         check8.push(q,r,s);
         check8 = check8.join(""); 
     
-    if ((check8 === "XXX") || (check8 === "OOO")) {
+        if ((check8 === "XXX") || (check8 === "OOO")) {
             console.log("TrickyWIN8")
             _win()
             return
             }
-        })()
-
-    // logic to call a tie
-    if (!gameBoardGrid.includes("") && (won == false)) {
-        alert ("tie")
+        }
+        
+        checkTie()
+        if (tie === false) {
+        win1()
+        win2()
+        win3()
+        win4()
+        win5()
+        win6()
+        win7()
+        win8()
+        }
     }
     
-    }
-    
-    const _win = () => {
+        const _win = () => {
         won = true
+        overlayResult.textContent = "The winner is `${currentPlayer}!`"
+        toggleWinOverlay()
         alert(`${currentPlayer}, you win! `)
-
-    }    
-    _createGameBoard()
-    _checkWin() 
-
+        }    
+        _createGameBoard()
+        _checkWin() 
+    
         
 })()
 
